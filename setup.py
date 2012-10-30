653c8fb16ff26be5b2e73a31344c458b67ef553a
Waylon Calabrese <waylon@ustudio.com>
2012-10-30 11:11:38 -0500

    added package details

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tornado_websocket_client',
+    version='0.0.1',
+    description='Tornado Web Socket Client',
+    author='misc.',
+    packages=[
+        "tornado_websocket_client"
+    ],
+    requires=[],
+)
